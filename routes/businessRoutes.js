de3b3291e90bf0ca252aac7f604668ffd8a76db8
Kyle Gichez <gichuremaina4@gmail.com>
2026-04-17 13:51:46 +0300

    Fix backend errors

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -4,13 +4,11 @@ const router = express.Router();
 const {
   getMyBusinessProfile,
   createBusinessWithAdmin,
-  createStaffUser,
 } = require("../controllers/businessController");
 const protect = require("../middleware/authMiddleware");
 const authorize = require("../middleware/roleMiddleware");
 
 router.get("/", protect, getMyBusinessProfile);
 router.post("/", protect, authorize("superadmin"), createBusinessWithAdmin);
-router.post("/staff", protect, authorize("superadmin"), createStaffUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -31,7 +31,6 @@ app.use("/api/payments",require('./routes/paymentRoutes'));
 app.use("/api/admin/dashboard",require('./routes/dashboardRoutes'));
 app.use("/api/admin/business",require('./routes/businessRoutes'));
 app.use("/api/admin/businesses",require('./routes/usersRoutes'));
-app.use("/api/admin/staff",require('./routes/businessRoutes'));
 app.use("/api/admin/users",require('./routes/usersRoutes'));
 app.use("/api/myprofile",require('./routes/businessRoutes'));
 app.use("/api/settings",require('./routes/usersRoutes'));
