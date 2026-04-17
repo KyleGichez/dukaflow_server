5d9ecf976365fc3a1289ce391e5e9ed98f43588e
Kyle Gichez <gichuremaina4@gmail.com>
2026-04-17 13:26:12 +0300

    Add more staff routes logic

--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const {
   getMyBusinessProfile,
   createBusinessWithAdmin,
+  createStaffUser,
 } = require("../controllers/businessController");
 const protect = require("../middleware/authMiddleware");
 const authorize = require("../middleware/roleMiddleware");
 
 router.get("/", protect, getMyBusinessProfile);
 router.post("/", protect, authorize("superadmin"), createBusinessWithAdmin);
+router.post("/", protect, authorize("superadmin"), createStaffUser);
 
 module.exports = router;
