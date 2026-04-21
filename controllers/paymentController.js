b9753f6eff07f89baf9d711c0beb4cacb98bf112
Kyle Gichez <gichuremaina4@gmail.com>
2026-04-21 17:12:36 +0300

    Fix: Add 4051509 account ref number

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -60,7 +60,7 @@ exports.stkPush = async (req, res) => {
         PartyB: process.env.MPESA_SHORTCODE,
         PhoneNumber: formattedPhone,
         CallBackURL: process.env.MPESA_CALLBACK_URL,
-        AccountReference: "DukaFlow Software",
+        AccountReference: "4051509",
         TransactionDesc: `Subscription for ${plan}`,
       },
       {
