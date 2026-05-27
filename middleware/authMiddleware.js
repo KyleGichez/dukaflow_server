693f77a930cd5d5002fa8709198763fcff59890b
Kyle Gichez <gichuremaina4@gmail.com>
2026-05-27 20:27:10 +0300

    Fix bug sold by

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,12 +1,21 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const auth = (req, res, next) => {
   const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "No token, authorization denied" });
+  if (!token)
+    return res.status(401).json({ message: "No token, authorization denied" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // This contains the id and ownerId you signed during login
+
+    const user = User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    req.user = user;
     next();
   } catch (err) {
     res.status(401).json({ message: "Token is not valid" });
