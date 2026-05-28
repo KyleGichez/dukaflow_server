fd2a7c7ef6f218efb6031eae2a1a57be31a1cfa5
Kyle Gichez <gichuremaina4@gmail.com>
2026-05-28 23:25:07 +0300

    add credit sales controller and routes2

--- routes/creditRoutes.js
+++ routes/creditRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const auth = require("../middleware/authMiddleware");
-const authorize = require("../middleware/authorize");
+const authorize = require("../middleware/roleMiddleware");
 
 const {
   createCredit,
