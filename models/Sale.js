4069e0fe3fb3021cf25a0f4b2672bfb27cbabc67
Kyle Gichez <gichuremaina4@gmail.com>
2026-05-30 14:21:12 +0300

    modify sale schema

--- models/Sale.js
+++ models/Sale.js
@@ -1,32 +1,78 @@
 const mongoose = require("mongoose");
 
+const saleItemSchema = new mongoose.Schema(
+  {
+    productId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+
+    quantitySold: {
+      type: Number,
+      required: true,
+    },
+
+    unitPrice: {
+      type: Number,
+      required: true,
+    },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+  },
+  { _id: false }
+);
+
 const saleSchema = new mongoose.Schema(
   {
-    productId: { 
-      type: mongoose.Schema.Types.ObjectId, 
-      ref: 'Product', 
-      required: true 
+    items: {
+      type: [saleItemSchema],
+      required: true,
     },
-    date: { type: Date, required: true},
-    quantitySold: { type: Number, required: true },
-    unitPrice: { type: Number},
-    totalPrice : {type: Number, required: true},
-    units: {type: String},
-    paymentMethod: { type: String, required: true },
+
+    totalPrice: {
+      type: Number,
+      required: true,
+    },
+
+    amountPaid: {
+      type: Number,
+      default: 0,
+    },
+
+    balance: {
+      type: Number,
+      default: 0,
+    },
+
+    paymentMethod: {
+      type: String,
+      required: true, // Cash | M-Pesa | Credit
+    },
+
+    paymentStatus: {
+      type: String,
+      enum: ["Paid", "Pending", "Partial"],
+      default: "Paid",
+    },
+
     soldBy: {
       type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
+      ref: "User",
     },
+
     businessId: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Business",
       required: true,
     },
-    createdAt: { type: Date, default: Date.now },
-    paymentStatus: {
-      type: String,
-      enum: ["Paid", "Pending", "Partial"],
-      default: "Paid",
+
+    date: {
+      type: Date,
+      default: Date.now,
     },
   },
   { timestamps: true }
